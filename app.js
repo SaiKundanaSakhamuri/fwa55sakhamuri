956a242fd556bf39d209a2354dca41b66e07a67b
kundanaSakhamuri98 <s545248@nwmissouri.edu>
2021-10-27 16:23:00 -0500

    updating app.js file

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var myDataRouter = require('./routes/mydata')
 var usersRouter = require('./routes/users');
-var mydataRouter= require('./routes/mydata');
 
 var app = express();
 
@@ -21,8 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/mydata', myDataRouter)
 app.use('/users', usersRouter);
-app.use('/mydata', mydataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,7 +34,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+ 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
