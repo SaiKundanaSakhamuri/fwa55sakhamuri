feb31cdde545802079fa7e8810f12071a2b8a678
kundanaSakhamuri98 <s545248@nwmissouri.edu>
2021-10-27 15:55:18 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Sai Lakshmi Kundana Sakhamuri' });
+router.get("/", function (req, res, next) {
+  res.render("mydata", {
+    title: "Sai Lakshmi Kundana Sakhamuri",
+    fact: "I hate cats",
+  });
 });
 
 module.exports = router;
